4ccedee79d2c939337d4ac446d917624aa3e994b
Weiry <gonweiry@gmail.com>
2022-08-22 11:23:18 +0000

    Add Error()

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void main(void){
+void error(char *argv);
 
+void main(int argc, char *argv[]){
+
+	/*	argv[0] = program.c
+		argv[1]	= option{-c, -d}
+		argv[2] = k
+		argv[3] = value of k
+		argv[4] = file input
+		argv[5] = file output
+	*/
+
+	FILE *file_1, *file_2;
+
+	if(! (strcmp(*(argv+1), "-c"))){
+
+		printf("[+]Encypt...\n");
 	
+	}else if(! (strcmp(*(argv+1), "-d"))){
+
+		printf("[+]Decrypt...\n");
 	
+	}else{
+
+		error(*	argv);
+	}
+
+}
+
+void error(char *argv){
+
+	printf("\n");
+
+	printf("[-] Syntax Error!\n");
+	printf("[*] Example:\n");
+	printf("Encrypt:\n\t %s -c -k <number> <file_imput> <file_output>\n", (argv));
+	printf("Decrypt:\n\t %s -d -k <number> <file_imput> <file_output>\n", (argv));
 }
