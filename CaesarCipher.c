b32a6e8310236ff58aa60d67020d475e90bb9d1b
Weiry <gonweiry@gmail.com>
2022-08-22 12:30:26 +0000

    Add Syntax()

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -2,32 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+int syntax(char *argv);
 void error(char *argv);
 
 void main(int argc, char *argv[]){
 
-	/*	argv[0] = program.c
+	/*	argv[0] = program
 		argv[1]	= option{-c, -d}
-		argv[2] = k
-		argv[3] = value of k
+		argv[2] = k OR n
+		argv[3] = value of argv[2]
 		argv[4] = file input
 		argv[5] = file output
 	*/
 
 	FILE *file_1, *file_2;
 
-	if(! (strcmp(*(argv+1), "-c"))){
+	int opt = syntax(*(argv));
 
-		printf("[+]Encypt...\n");
-	
-	}else if(! (strcmp(*(argv+1), "-d"))){
+	switch(opt){
+		case 1:
+			printf("[+]Encypt...\n");
+		break;
 
-		printf("[+]Decrypt...\n");
-	
-	}else{
+		case 2:
+			printf("[+]Decrypt...\n");
+		break;
 
-		error(*	argv);
+		default:
+			error(*argv);
 	}
+
+}
+
+int syntax(char *argv){
+
+	//printf("> %s", (*argv));
+
+
+	if( !(strcmp((argv+1), "-c")) ){
+
+		if( !(strcmp((argv+2), "-k")) || !(strcmp((argv+2), "-n"))){
+			printf("43\n");
+		}
+	
+	}else if( !(strcmp((argv+1), "-d")) ){
+
+		if( !(strcmp((argv+2), "-k")) || !(strcmp((argv+2), "-n"))){
+			printf("49\n");
+
+		}
+
+	}else
+		return -1;
+
 
 }
 
