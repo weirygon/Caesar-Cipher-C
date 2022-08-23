7d59a96da6f48fdc0bf97b8e4fbe153d7e9644fb
Weiry <gonweiry@gmail.com>
2022-08-23 09:44:43 +0000

    Fixed Syntax()

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int syntax(char **argv);
+void encrypt(FILE *in, FILE *out);
 int isNumber(char *argv);
+int syntax(char **argv);
 void error(char *argv);
 
 void main(int argc, char *argv[]){
@@ -16,13 +17,35 @@ void main(int argc, char *argv[]){
 		argv[5] = file output
 	*/
 
-	FILE *file_1, *file_2;
+	FILE *file_in, *file_out;
 
 	int opt = syntax((argv));
 
 	switch(opt){
 		case 1:
+
 			printf("[+]Encypt...\n");
+			file_in = fopen(argv[4], "r");
+			
+			if(!file_in){
+				printf("[-]The file %s cannot be open!\n", argv[4]);
+				exit(1);
+			}
+
+			file_out = fopen(argv[5], "w");
+
+			if(!file_out){
+
+				printf("[-] The file %s cannot be create!\n", argv[5]);
+				exit(1);
+			}
+
+			encrypt(file_in, file_out);
+			fclose(file_in);
+			fclose(file_out);
+			file_in = NULL;
+			file_out = NULL;
+
 		break;
 
 		case 2:
@@ -35,7 +58,37 @@ void main(int argc, char *argv[]){
 
 }
 
+void encrypt(FILE *in, FILE *out){
+
+	char aux;
+
+	printf("============ENCRYPT============\n");
+	printf("%p \n", in);
+
+	while( !(feof(in)) ){
+	//	fscanf(in, "%s", stdin);
+	//	printf("%s \n", stdin);
+	}
+}
+
+int isNumber(char *argv){
+
+	for(int i=0; argv[i]!= 0; i++){
+
+		if(argv[i] < 48 || argv[i] > 57)
+			return 0;
+	}
+
+	return 1;
+
+}
+
 int syntax(char **argv){
+
+	for(int i=0; i<6; i++){
+		if(!(argv[i]))
+			return -1;
+	}
 
 	
 	if( !(strcmp(argv[1], "-c")) ){
@@ -52,7 +105,7 @@ int syntax(char **argv){
 	}else if( !(strcmp(argv[1], "-d")) ){
 
 		if( !(strcmp(argv[2], "-k")) || !(strcmp(argv[2], "-n"))){
-			
+
 			if(isNumber(argv[3]))
 				return 1;
 			else
@@ -62,19 +115,6 @@ int syntax(char **argv){
 
 	}else
 		return -1;
-
-
-}
-
-int isNumber(char *argv){
-
-	for(int i=0; argv[i]!= 0; i++){
-
-		if(argv[i] < 48 || argv[i] > 57)
-			return 0;
-	}
-
-	return 1;
 
 }
 
